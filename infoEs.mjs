6c2628060b2abb58531e91529b99d7c4fdf6598e
Dpalme <diegoPalmerin24@gmail.com>
2022-02-07 22:27:00 -0600

    initial spanish test

---
+++ infoEs.mjs
@@ -0,0 +1,13 @@
+export let plants = [
+    {
+        name: "Narciso",
+        info: `(Narcissus 'Actaea') Es una planta que nace de bulbos y pertenece a la familia Amaryllidaceae. Esta flor originaria de las cuencas mediterráneas, Asia y Áfria septentrional debe su nombre a la mitología griega. Según el mito, Narciso era un joven que se enamoró de si mismo al mirarse en un río y murió ahogado al lanzarse al agua. En ese mismo lugar creció la flor. Se relaciona con el renacer, nuevos comienzos y a un amor no correspondido.`,
+        img: "./img/narciso.jpg"
+    },
+    {
+        name: 'Ipomoea violacea',
+        info: `Pertenece a la familia de las Convolvuláceas. Es una planta originaria de centroamérica y México. Representa vivir sin miedo a expresar lo que uno siente y sin limitaciones. Cada mañana, muestra su belleza y firmeza y al caer el sol demuestra sus debilidades, cansancio y dudas. También, sus semillas son usadas como alucinógeno.`,
+        img: './img/narciso.jpg'
+    }
+]
+ // TODO complete list
